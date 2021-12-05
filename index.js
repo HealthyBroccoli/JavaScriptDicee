3bb41bfeebbd516de43b90118cecfb9b0d484ff2
M1Abrams <80999493+HealthyBroccoli@users.noreply.github.com>
2021-12-05 13:38:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 var player1 = Math.floor(Math.random() * 6) + 1;
 var player2 = Math.floor(Math.random() * 6) + 1;
-const player = [player1, player2];
 
-for (var i = 0; i < 2; i++) {
-  switch (player[i]) {
-    case 1:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice1.png";
-      break;
-    case 2:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice2.png";
-      break;
-    case 3:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice3.png";
-      break;
-    case 4:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice4.png";
-      break;
-    case 5:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice5.png";
-      break;
-    case 6:
-      document.getElementsByClassName("dice")[i].getElementsByClassName("img")[0].src = "images/dice6.png";
-      break;
-    default:
-      break;
-  }
-}
+const p1dice = 'images/dice' + player1 + '.png';
+const p2dice = 'images/dice' + player2 + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', p1dice)
+document.querySelectorAll('img')[1].setAttribute('src', p2dice)
 
 if (player1 > player2) {
   console.log("Player 1 Wins");
